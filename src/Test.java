59c02e2d28c7e1973e35b9b78b3138eb92910114
Chris Dessonville <chris@chris-macbook.local>
2012-12-02 15:16:29 -0500

    Added log4j.  Setup demo logging to console.

--- src/Test.java
+++ src/Test.java
@@ -4,11 +4,15 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.handler.AbstractHandler;
 
 public class Test extends AbstractHandler {
+    static Logger logger = Logger.getLogger(Test.class);
+
     @Override
     public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response) throws IOException,
             ServletException {
@@ -16,9 +20,13 @@ public class Test extends AbstractHandler {
         response.setStatus(HttpServletResponse.SC_OK);
         baseRequest.setHandled(true);
         response.getWriter().println("<h1>Hello World</h1>");
+        logger.info(request.getPathInfo());
     }
 
     public static void main(String[] args) throws Exception {
+        // Set up a simple configuration that logs on the console.
+        BasicConfigurator.configure();
+
         Server server = new Server(8080);
         server.setHandler(new Test());
 
