a9ef99569c06c60b4a52867be3b5a618dbf7c001
Aaron Prindle <aaprindle@gmail.com>
2012-12-11 21:13:12 -0500

    first draft of PreParser validRegularExpression method, attempting testing

--- src/com/awesomecat/jslogger/PreParser.java
+++ src/com/awesomecat/jslogger/PreParser.java
@@ -1,5 +1,8 @@
 package com.awesomecat.jslogger;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class PreParser {
 	
 	/**
@@ -40,12 +43,65 @@ public class PreParser {
          * 		NOTE2: Be careful to not ban \* or \+... those are valid escaped characters
          * 
          * 8) At this point, I think everything should be valid and not overly inefficient
-         * 
+         * 9) Make sure it is a valid expression based on the regular expression validator(CHRIS)
          * 
          * 
          * NOTE: What about if they escape that last /?   (i.e. /hi\/  wouldn't be valid)
          */ 
-        return false;
+    	if (re != null && !re.isEmpty()) {
+    		  return false;
+    	}
+    	int flag_index=0;
+    	for(int i= (re.length()-1);i>=0; i-- ){
+    		String cur_char = re.substring(i,i+1);
+    		if(!isValidFlag(cur_char)){
+    			return false;
+    		}
+    		if(cur_char == "/"){
+    			flag_index = i; //is this right?
+    			break;
+    		}
+    	}
+    	String flagless = re.substring(flag_index, flag_index+1);
+    	if(flagless.substring(0,1) != "/" 
+    		&& flagless.substring(flagless.length()-1,flagless.length()) != "/"
+    		&& flagless.substring(flagless.length()-2,flagless.length()-1)!= "\\"){
+    		return false;
+    	}
+    	String no_slash = flagless.substring(1,flagless.length()-1); //is this right?
+    	if(no_slash.substring(0,1) != "^" 
+    		&& no_slash.substring(no_slash.length()-1,no_slash.length()) != "$"){
+    		return false;
+    	}
+    	String no_anchor = no_slash.substring(1,no_slash.length()-1); //is this right?
+    	for(int i= 0;i<no_anchor.length(); i++ ){
+    		String cur_char = no_anchor.substring(i,i+1);
+    		String before_char = no_anchor.substring(i-1,i);
+    		if((cur_char == "+" || cur_char == "*") && before_char!="\\"){
+    				return false;
+    		}
+    		if(cur_char == "{" && before_char!="\\"){
+    			//2 bad cases: {,} and {#,}
+    			if(no_anchor.substring(i,i+3) == "{,}"){
+    				return false;
+    			}
+    			Pattern p = Pattern.compile("\\{\\d+,\\}");
+    			if(matchesPattern(p,no_anchor.substring(i))){ //might be slow
+    				return false;
+    			}
+    		}
+    	}
+    	return true;
     }
+    //ADDED
+    private static boolean matchesPattern(Pattern p,String sentence) {
+        Matcher m = p.matcher(sentence);
+
+        if (m.find()) {
+          return true;
+        }
+
+        return false;
+      }
 
 }
