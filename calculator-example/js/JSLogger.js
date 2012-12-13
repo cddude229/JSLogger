6d94ab81eba80e900f89756a1f8d1eade7d2ead6
Chris Dessonville <chris@chris-macbook.local>
2012-12-13 16:40:18 -0500

    Updating base JSLogger code

--- calculator-example/js/JSLogger.js
+++ calculator-example/js/JSLogger.js
@@ -1,13 +1,16 @@
-function Logger(url){
-    // Add object properties like this
-    this.url = url;
-};
-// Add methods like this.
-Logger.prototype.log = function(message, logid){
-    $.ajax({
-	       type: 'GET',
-	       url: this.url,
-	       data: {message: message, logid: logid, jsLogger:1},
-	       contentType: 'application/json; charset=utf-8'
-	   });
+var Logger = function(url){
+	return {
+		log: function(message, logid){
+			$.ajax({
+				type: 'GET',
+				url: url,
+				data: {
+					message: message,
+					logid: logid,
+					jsLogger: 1
+				},
+				contentType: 'application/json; charset=utf-8'
+			});
+		}
+	}
 };
