22a7e740e76917862b9e2514f5646bfb05faca59
Aaron Prindle <aaprindle@gmail.com>
2012-12-14 12:50:46 -0500

    working on tester.html

--- TESTINPUT/basic_test.js
+++ TESTINPUT/basic_test.js
@@ -2,8 +2,8 @@
 * @valid-duration 5
 * @run-once true
 * @window-size 2
-* @validate /^basic_test$/sim
-* @id 5
+* @validate /^generating_valid_error$/sim
+* @id validError
 #**/
 
-logger.log("blah", "$id=5");
+logger.log("generating_valid_error", "$id=validError");
